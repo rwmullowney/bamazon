5bbb1639d553a075f18aa760a252b1625d1bae36
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-04-20 20:35:11 -0500

    Completes JS code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,12 +15,8 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     console.log("connected as id " + connection.threadId + "\n");
-// });
 
-
+// Displays the table of products
 var displayProducts = function () {
     var query = "SELECT * FROM products";
     connection.query(query, function (err, res) {
@@ -29,77 +25,93 @@ var displayProducts = function () {
     })
 }
 
-var correctItemID = true;
-var itemID;
+// Initiates the prompt to the user
 var promptUser = function () {
-    
     inquirer.prompt([{
-                name: "itemID",
-                message: "Please enter the ID of the item you would like to purchase: "
-            }
-            // , {
-            //     name: "quantity",
-            //     message: "How many would you like to buy? "
-            // }
-        ])
+            name: "ID",
+            message: "Please enter the ID of the item you would like to purchase: "
+        }])
         .then(function (answer) {
-            itemID = answer.itemID
             checkItemID(answer);
-            if (!correctItemID){
-                promptUser();
-            }
-            else{quantityPrompt()}
         })
 };
 
-
+// Declares variable for the item ID for later reference
+var itemID;
+// Determines whether the item ID entered is valid
 var checkItemID = function (answer) {
     connection.query("SELECT * FROM products WHERE ?", {
-        item_id: itemID
-    },
+            item_id: answer.ID
+        },
         function (err, res) {
-            if (answer.itemQuantity > res[0].stock_quantity) {
-                console.log("That's more than our stock!");
-                quantityPrompt();
-            }
-            console.log("Item(s) ordered!")
-        })
-}
 
-
-var quantityPrompt = function(){
-    inquirer.prompt([{
-        name: "itemQuantity",
-        message: "How many would you like to purchase? "
-    }])
-    .then(function(answer){
-        checkQuantity(answer);
-    })
-};
-
-var checkQuantity = function (answer) {
-    connection.query("SELECT * FROM products WHERE ?", {
-        item_id: answer.itemID
-    },
-        function (err, res) {
+            // Checks if resulting array is empty, indicating it's an invalid item ID (i.e. there's no associated data with it)
             if (res.length === 0) {
                 console.log("Not a valid item ID.");
 
-                // Restart the user prompt
-                // Why is this not currently working?
-                // promptUser();
-                correctItemID = false;
-                return correctItemID;
+                // Repeats initial prompt if not valid
+                promptUser();
+            } else {
+                // Assigns the item ID to a variable if valid
+                itemID = res[0].item_id;
+
+                // Asks quantity user wants to purchase if valid item ID
+                quantityPrompt();
             }
-            console.log(res[0].stock_quantity);
-            // promptUser();
         })
 };
-// Since I'm running out of time at work, here's what I think needs to be done moving forward:
-// First have the inquirer ask the ID of the item to be purchased.  If it's not there, i.e.
-// if the array that's returned has a length of zero, present text saying that item is not found and to try again.
-// Probably have a function to display the table if it gets pushed up too high (but that maybe shouldn't be a problem).
-// 
-//
-displayProducts()
+
+
+// Asks quantity user wants to purchase if valid item ID
+var quantityPrompt = function () {
+    inquirer.prompt([{
+            name: "quantity",
+            message: "How many would you like to purchase? "
+        }])
+        .then(function (answer) {
+            checkQuantity(answer);
+        })
+};
+
+// Checks if store supply means quantity requested
+var checkQuantity = function (answer) {
+    connection.query("SELECT * FROM products WHERE ?", {
+            item_id: itemID
+        },
+        function (err, res) {
+            if (answer.quantity > res[0].stock_quantity) {
+                console.log("That's more than our stock!");
+
+                // Re-prompts the user for the quantity they'd like to purchase if too much
+                quantityPrompt();
+            } else {
+
+                // Declares variables for the quantity ordered, quantity after ordered, and the order cost
+                quantityAfterOrder = res[0].stock_quantity - answer.quantity;
+                var orderSum = (res[0].price * answer.quantity);
+
+                console.log("Item(s) ordered!")
+                console.log("Cost of order: $" + orderSum)
+                submitOrder()
+            }
+        })
+}
+
+var quantityAfterOrder;
+// Updates the quantity in the database to reflect the order
+var submitOrder = function () {
+    connection.query("UPDATE products SET ? WHERE ?", [{
+            stock_quantity: quantityAfterOrder
+        }, {
+            item_id: itemID
+        }],
+        function (err, res) {
+            connection.end()
+        })
+
+
+}
+
+// Initiates functions to start program
+displayProducts();
 promptUser();
