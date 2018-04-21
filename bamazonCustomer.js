8b31dc1d51e76a84b9f50d70a3beeaea974d03c3
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-04-20 19:19:21 -0500

    Adds much more JS code, but there's issues with the inquirer prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,9 +21,85 @@ var connection = mysql.createConnection({
 // });
 
 
+var displayProducts = function () {
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        console.log()
+        console.table(res);
+    })
+}
 
-var query = "SELECT * FROM products"
-connection.query(query, function (err, res) {
-    console.table(res)
-})
+var correctItemID = true;
+var itemID;
+var promptUser = function () {
+    
+    inquirer.prompt([{
+                name: "itemID",
+                message: "Please enter the ID of the item you would like to purchase: "
+            }
+            // , {
+            //     name: "quantity",
+            //     message: "How many would you like to buy? "
+            // }
+        ])
+        .then(function (answer) {
+            itemID = answer.itemID
+            checkItemID(answer);
+            if (!correctItemID){
+                promptUser();
+            }
+            else{quantityPrompt()}
+        })
+};
 
+
+var checkItemID = function (answer) {
+    connection.query("SELECT * FROM products WHERE ?", {
+        item_id: itemID
+    },
+        function (err, res) {
+            if (answer.itemQuantity > res[0].stock_quantity) {
+                console.log("That's more than our stock!");
+                quantityPrompt();
+            }
+            console.log("Item(s) ordered!")
+        })
+}
+
+
+var quantityPrompt = function(){
+    inquirer.prompt([{
+        name: "itemQuantity",
+        message: "How many would you like to purchase? "
+    }])
+    .then(function(answer){
+        checkQuantity(answer);
+    })
+};
+
+var checkQuantity = function (answer) {
+    connection.query("SELECT * FROM products WHERE ?", {
+        item_id: answer.itemID
+    },
+        function (err, res) {
+            if (res.length === 0) {
+                console.log("Not a valid item ID.");
+
+                // Restart the user prompt
+                // Why is this not currently working?
+                // promptUser();
+                correctItemID = false;
+                return correctItemID;
+            }
+            console.log(res[0].stock_quantity);
+            // promptUser();
+        })
+};
+// Since I'm running out of time at work, here's what I think needs to be done moving forward:
+// First have the inquirer ask the ID of the item to be purchased.  If it's not there, i.e.
+// if the array that's returned has a length of zero, present text saying that item is not found and to try again.
+// Probably have a function to display the table if it gets pushed up too high (but that maybe shouldn't be a problem).
+// 
+//
+displayProducts()
+promptUser();
